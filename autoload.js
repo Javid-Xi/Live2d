db8a970e7c1ab8f7db5a9ee3c54f99969ce4aa95
Javid-Xi <jiaweixi1012@163.com>
2020-04-08 19:19:35 +0800

    update

--- autoload.js
+++ autoload.js
@@ -1,6 +1,7 @@
 // 注意：live2d_path 参数应使用绝对路径
-const live2d_path = "https://cdn.jsdelivr.net/gh/Javid-Xi/live2d@latest/";
-const tips_path = "https://cdn.jsdelivr.net/gh/Javid-Xi/live2d@latest/";
+const live2d_path = "https://cdn.jsdelivr.net/gh/Javid-Xi/Live2d@latest/";
+const tips_path = "https://cdn.jsdelivr.net/gh/Javid-Xi/Live2d@latest/";
+// const live2d_path = "";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -26,21 +27,15 @@ function loadExternalResource(url, type) {
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
-if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.min.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
 		initWidget({
-			waifuPath: live2d_path + "waifu-tips.json",
+			waifuPath: tips_path + "waifu-tips.json",
 			apiPath: "https://live2d.fghrsh.net/api/",
 			//cdnPath: "https://cdn.jsdelivr.net/gh/fghrsh/live2d_api/"
 		});
 	});
-}
-// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
-// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
-// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
-
 console.log();
